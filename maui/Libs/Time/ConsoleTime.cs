b3c0f2845e1c80bd626879bfc3c875db28b0f1eb
Daniel Sierpiński <33436839+sierpinskid@users.noreply.github.com>
2024-02-13 15:52:50 +0100

    Add libraries for .NET MAUI

---
+++ maui/Libs/Time/ConsoleTime.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StreamChat.Libs.Time
+{
+    public class ConsoleTime : ITimeService
+    {
+        public ConsoleTime()
+        {
+            _startTime = DateTime.UtcNow;
+        }
+
+        public float Time => (float)(DateTime.UtcNow - _startTime).TotalSeconds;
+        
+        public float DeltaTime => 0.1f;
+        
+        private readonly DateTime _startTime;
+    }
+}
